4af203b1baa1ab56d0891a3e703657f700a22d82
JulianL76 <crisjulian_07@outlook.com>
2026-09-15 00:13:51 -0500

    Ajustes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group, User
 from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 from .models import Negocio, PerfilUsuario
+
+# Django registra User y Group con sus propias clases admin, que no heredan de
+# Unfold: por eso su changelist perdía la barra de acciones y el botón "Añadir".
+# Se reemplazan por versiones basadas en ModelAdmin de Unfold.
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
 
 
 @admin.register(Negocio)
